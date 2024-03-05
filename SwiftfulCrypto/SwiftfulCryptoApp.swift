7e610eb04daed76d9c739ea7c183bea4dc3ecb05
Nithya Vasudevan <nithya1vasudevan@gmail.com>
2024-03-05 11:19:36 +0100

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Nithya Vasudevan on 05.03.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
